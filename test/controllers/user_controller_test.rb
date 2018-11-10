f0f79732caf1347683d763aad60218ab6e1b9714
abhinayak <abhishek.nayak@mountblue.io>
2018-11-10 16:40:00 +0530

    Refactored code

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Given the username and password, should be able to create new user" do
+    post 'get', params:{
+      username: 'test1',
+      password: 'test@1'
+    }
+    assert_response 200
+  end
+
+  test "Given the credentials, should be able to create new user" do
+    post 'create', params:{
+      username: 'test1',
+      password: 'test@1'
+    }
+    assert_response 200
+  end
 end
